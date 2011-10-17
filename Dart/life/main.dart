b3d9ff3c117a6d5e73d982a142daf694a862c43f
Seldaiendil <seldaiendil2@gmail.com>
2011-10-17 19:45:23 +0200

    Added things (don't ask me what)

--- Dart/life/main.dart
+++ Dart/life/main.dart
@@ -1,2 +1,21 @@
+#import('physic/Vector.dart');
+#import('physic/Force.dart');
+
 int main() {
+	var force = new Force.empty();
+	print(force);
+
+	/*
+	var vec = new Vector(2, 4);
+	print(vec);
+	print(vec.angle);
+	print(vec += 1);
+	print(vec += new Vector(7, 5));
+	print(-vec);
+	print(vec.angle);
+	print(vec.round(2));
+	print(vec);
+	print(vec.abs());
+	print(vec);
+	*/
 }
