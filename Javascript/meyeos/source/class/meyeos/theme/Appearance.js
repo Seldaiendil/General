41db704a055884b629d48f1e20389d19dcb3b952
A. Matías Quezada <matias.quezada@eyeos.org>
2012-01-10 18:38:27 +0100

    Added meyeos project

---
+++ Javascript/meyeos/source/class/meyeos/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("meyeos.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
